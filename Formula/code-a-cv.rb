de6a7035fae8a009f05941c59266ca407213aaa1
voxvanhieu <voxvanhieu@gmail.com>
2026-08-31 01:18:31 +0700

    code-a-cv 0.1.1

---
+++ Formula/code-a-cv.rb
@@ -0,0 +1,22 @@
+class CodeACv < Formula
+  desc "Build CVs from Markdown or structured data"
+  homepage "https://github.com/voxvanhieu/code-a-cv"
+  url "https://github.com/voxvanhieu/code-a-cv/releases/download/v0.1.1/source.tar.gz"
+  sha256 "57259c2f568e35670c4d9a3e066d44ef038cb3842bf4fd8b403250593984c34c"
+  license "MIT"
+  head "https://github.com/voxvanhieu/code-a-cv.git", branch: "main"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", "--profile=dist", *std_cargo_args(path: "crates/cac")
+  end
+
+  test do
+    system bin/"cac", "init"
+    assert_path_exists testpath/"cv.md"
+
+    system bin/"cac", "build", "cv.md", "--format", "html", "--output", "dist"
+    assert_path_exists testpath/"dist/cv.html"
+  end
+end
