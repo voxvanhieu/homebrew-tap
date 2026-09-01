286b7d5019a22570b0b81899b62ac9ce79f22dfd
voxvanhieu <voxvanhieu@gmail.com>
2026-09-01 19:47:40 +0700

    code-a-cv 0.2.0

--- Formula/code-a-cv.rb
+++ Formula/code-a-cv.rb
@@ -1,16 +1,10 @@
 class CodeACv < Formula
   desc "Build CVs from Markdown or structured data"
   homepage "https://github.com/voxvanhieu/code-a-cv"
-  url "https://github.com/voxvanhieu/code-a-cv/releases/download/v0.1.1/source.tar.gz"
-  sha256 "57259c2f568e35670c4d9a3e066d44ef038cb3842bf4fd8b403250593984c34c"
+  url "https://github.com/voxvanhieu/code-a-cv/releases/download/v0.2.0/source.tar.gz"
+  sha256 "09648823f4949ffc5de298f653a5beddfd993cdaff9dddb9a450355c148dbd8c"
   license "MIT"
   head "https://github.com/voxvanhieu/code-a-cv.git", branch: "main"
-
-  bottle do
-    root_url "https://github.com/voxvanhieu/homebrew-tap/releases/download/code-a-cv-0.1.1"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "8260c43032cdbafcdd3712059b2d8a9d6a992923ba4625855059627df0e99544"
-    sha256 cellar: :any,                 x86_64_linux: "6a2ad67d6b799f599e84ac1aff56728ef3417b79d92525e3074e0a15ce85e78d"
-  end
 
   depends_on "rust" => :build
 
