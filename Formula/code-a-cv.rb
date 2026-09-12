45d9dc45a3ba4f30ef53d9a799f7c97f8fefae08
voxvanhieu <17991469+voxvanhieu@users.noreply.github.com>
2026-09-12 12:41:51 +0000

    code-a-cv 0.3.0

--- Formula/code-a-cv.rb
+++ Formula/code-a-cv.rb
@@ -1,16 +1,10 @@
 class CodeACv < Formula
   desc "Build CVs from Markdown or structured data"
   homepage "https://github.com/voxvanhieu/code-a-cv"
-  url "https://github.com/voxvanhieu/code-a-cv/releases/download/v0.2.0/source.tar.gz"
-  sha256 "09648823f4949ffc5de298f653a5beddfd993cdaff9dddb9a450355c148dbd8c"
+  url "https://github.com/voxvanhieu/code-a-cv/releases/download/v0.3.0/source.tar.gz"
+  sha256 "49d6a2cbe2d449d9d47ef087d5b12167d6c4f9617d536877e6f1efd98953d2f2"
   license "MIT"
   head "https://github.com/voxvanhieu/code-a-cv.git", branch: "main"
-
-  bottle do
-    root_url "https://github.com/voxvanhieu/homebrew-tap/releases/download/code-a-cv-0.2.0"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "3be8ca1035083d5a503f0c51d42a4b43985c8891cb740ef485a1e894b33d5c8d"
-    sha256 cellar: :any,                 x86_64_linux: "38b5ca62d139ff966defbaafdbe9a6750c3298a0292492b7c5e141b8739fee54"
-  end
 
   depends_on "rust" => :build
 
@@ -22,7 +16,7 @@ class CodeACv < Formula
     system bin/"cac", "init"
     assert_path_exists testpath/"cv.md"
 
-    system bin/"cac", "build", "cv.md", "--format", "html", "--output", "dist"
-    assert_path_exists testpath/"dist/cv.html"
+    system bin/"cac", "build", "cv.md", "--format", "html"
+    assert_path_exists testpath/"offering/cv.html"
   end
 end
